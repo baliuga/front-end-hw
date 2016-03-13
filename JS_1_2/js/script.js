d413d30b407b75f805b60ed8ef26f3d1b3c7a6f4
lego666less <lego666less@gmail.com>
2016-03-13 13:43:29 +0200

    Update script.js

--- JS_1_2/js/script.js
+++ JS_1_2/js/script.js
@@ -1,29 +1,48 @@
+/* First Task */
 function isInt(x) {
  return x % 1 === 0;
 }
 
-function pow() {
- console.log('Task 1 execute result:');
- var result = 1;
- var base = prompt("Enter base.", "");
- var exponent = prompt("Enter exponent. Please, avoid floating and negative numbers.", "");
- if (exponent < 0 || !isInt(exponent)) {
-  alert("Unacceptable input! Please, try again.");
-  console.log("Failed");
- } else {
-  for (var i = 0; i < exponent; i++) {
-   result *= base;
-  }
-  console.log("Result = ", result);
- }
+function isPositive(x) {
+  return x > 0 ;
 }
 
-function authorization() {
- console.log('Task 2 execute result:');
+function pow(base, exponent) {
+ var result = 1;
+ if (!isPositive(exponent) || !isInt(exponent)) {
+  result = -1;
+} else {
+  for (var i = 0; i < exponent; i++) {
+   result *= base;
+ } 
+}
+return result;
+}
+
+function firstTaskMain() {
+ console.log('Task 1 execute result:'); 
+ var base = prompt("Enter base.", "");
+ var exponent = prompt("Enter exponent. Please, avoid floating and negative numbers.", "");
+ var result = pow(base, exponent);
+ if (result == -1) {
+  alert("Unacceptable input! Please, try again.");
+  console.log("Failed");
+} else {  
+  console.log("Result = ", result);
+}
+}
+/* Second Task */
+function fillArray() {
  var arrNames = [];
  for (var i = 0; i < 5; i++) {
   arrNames.push(prompt("Enter name.", ""));
- }
+}
+return arrNames;
+}
+
+function secondTaskMain() {
+ console.log('Task 2 execute result:');
+ var arrNames = fillArray(); 
  var userName = prompt("Please, enter YOUR name.", "");
  var flag = false;
  for (var i = 0; i < arrNames.length; i++) {
@@ -32,10 +51,10 @@ function authorization() {
    console.log("Authorization succeed");
    flag = true;
    break;
-  }
  }
- if (!flag) {
+}
+if (!flag) {
   alert("Oops, you are not in the list!");
   console.log("Authorization failed");
- }
+}
 }
