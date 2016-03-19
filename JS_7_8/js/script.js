8ca5df24946de528db31a6c6474e1c7c6fc0861d
lego666less <lego666less@gmail.com>
2016-03-19 19:17:09 +0200

    add lab 7_8

---
+++ JS_7_8/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+	$(".myTooltip").css('opacity','0');
+
+	$('.first .btn').on('click', function(e)  {
+		$(".myTooltip").stop().animate({ opacity: 1 });
+		e.preventDefault();
+	});
+
+	
+	$(".first .block").hover(
+		function() {
+			$(this).find(".myTooltip").stop().animate({ opacity: 1 });
+		},
+		function() {
+			$(this).find(".myTooltip").stop().animate({ opacity: 0 }); 
+		});
+
+	$('.second .tab-links a').on('click', function(e)  {
+	  var currentAttrValue = $(this).attr('href'); 
+      //$('.second ' + currentAttrValue).show().siblings().hide();
+      $('.second ' + currentAttrValue).fadeIn(500).siblings().hide();
+      $(this).parent('li').addClass('active').siblings().removeClass('active'); 
+      e.preventDefault();
+  });
+
+});
