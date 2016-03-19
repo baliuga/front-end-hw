824339ab38baedc9251127ce346c042858bb38bd
lego666less <lego666less@gmail.com>
2016-03-19 19:19:12 +0200

    Update script.js

--- JS_7_8/js/script.js
+++ JS_7_8/js/script.js
@@ -17,11 +17,11 @@ $(document).ready(function() {
 		});
 
 	$('.second .tab-links a').on('click', function(e)  {
-	  var currentAttrValue = $(this).attr('href'); 
-      //$('.second ' + currentAttrValue).show().siblings().hide();
-      $('.second ' + currentAttrValue).fadeIn(500).siblings().hide();
-      $(this).parent('li').addClass('active').siblings().removeClass('active'); 
-      e.preventDefault();
+		var currentAttrValue = $(this).attr('href'); 
+      		//$('.second ' + currentAttrValue).show().siblings().hide();
+      		$('.second ' + currentAttrValue).fadeIn(500).siblings().hide();
+      		$(this).parent('li').addClass('active').siblings().removeClass('active'); 
+        	e.preventDefault();
   });
 
 });
